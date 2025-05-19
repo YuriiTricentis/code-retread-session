054ee40c65ee9ae652a67c6243a2c6cb45c67d66
Y.Y.F <y.fotul@tricentis.com>
2025-05-19 15:19:49 +0200

    Add more tests for robustness

--- string-to-uppercase.test.js
+++ string-to-uppercase.test.js
@@ -6,3 +6,18 @@ test('toUpperCase {STRINGTOUPPER[hello]}', () => {
   // Test with a string containing STRINGTOUPPER token
   expect(toUpperCase('{STRINGTOUPPER[hello]}')).toBe('HELLO');
 });
+
+test('toUpperCase {STRINGTOUPPER[hello]} with other text', () => {
+  // Test with a string containing STRINGTOUPPER token
+  expect(toUpperCase('This is a test {STRINGTOUPPER[hello]}')).toBe('THIS IS A TEST HELLO');
+});
+
+test('toUpperCase {STRINGTOUPPER[hello]} with other text on end', () => {
+  // Test with a string containing STRINGTOUPPER token
+  expect(toUpperCase('{STRINGTOUPPER[hello]} this is a test')).toBe('HELLO THIS IS A TEST');
+});
+
+test('toUpperCase {STRINGTOUPPER[hello]} with other text on end and start', () => {
+  // Test with a string containing STRINGTOUPPER token
+  expect(toUpperCase('This is a test {STRINGTOUPPER[hello]} this is a test')).toBe('THIS IS A TEST HELLO THIS IS A TEST');
+});
