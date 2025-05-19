7dcfbd2fceb4cdb10c6108d43e5e19e1e362ca87
Y.Y.F <y.fotul@tricentis.com>
2025-05-19 15:18:16 +0200

    Refactor a code to don't mutate parameter

--- string-to-uppercase.js
+++ string-to-uppercase.js
@@ -1,14 +1,16 @@
 export function toUpperCase(str) {
   // Check if the input is a string
-  str = str.replace(/{STRINGTOUPPER\[(.*?)\]}/g, '$1');
-  if (str.includes('{STRINGTOUPPER[')) {
-    return str;
+
+  let cleanStr = str.replace(/{STRINGTOUPPER\[(.*?)\]}/g, '$1');
+
+  if (cleanStr.includes('{STRINGTOUPPER[')) {
+    return cleanStr;
   }
 
-  if (typeof str !== 'string') {
+  if (typeof cleanStr !== 'string') {
     throw new TypeError('Input must be a string');
   }
 
   // Convert the string to uppercase
-  return str.toUpperCase();
+  return cleanStr.toUpperCase();
 }
